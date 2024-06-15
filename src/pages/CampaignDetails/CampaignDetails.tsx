3002c1280f2b1b1f85c14915e83ed391d760d385
BalanaguYashwanth <balanaguyashwanth@gmail.com>
2024-06-15 14:35:59 +0530

    Added optional chaining to campaign details props

--- src/pages/CampaignDetails/CampaignDetails.tsx
+++ src/pages/CampaignDetails/CampaignDetails.tsx
@@ -95,8 +95,8 @@ const CampaignDetails = () => {
 
                     <CampaignCard
                         key={1}
-                        imageSrc={campaign.banner}
-                        category={campaign.category}
+                        imageSrc={campaign?.banner}
+                        category={campaign?.category}
                         clicks={(campaign?.validClicks + campaign?.invalidClicks) || 0}
                         validclicks={campaign.validClicks}
                         title={campaign.campaignName}
@@ -110,8 +110,8 @@ const CampaignDetails = () => {
                         description={campaign.description}
                         url={campaign.originalUrl}
                         campaignInfoAddress={campaign.campaignInfoAddress}
-                        togglePopUp={() => togglePopUp(campaign.title)}
-                        popUp={activePopUp === campaign.title}
+                        togglePopUp={() => togglePopUp(campaign?.title)}
+                        popUp={activePopUp === campaign?.title}
                         handleShareUrl={toggleShareLink}
                         companyXProfile={campaign?.companyXProfile}
                         campaignvideolink={campaign?.campaignvideolink}
